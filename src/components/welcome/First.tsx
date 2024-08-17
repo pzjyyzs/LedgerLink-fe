b094130112ae2f4da7bdba6f6197f2d815a19c0f
pzjyyzs <463669395@qq.com>
2024-08-17 15:52:59 +0800

    rename

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,25 +1,26 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './layout.module.scss';
 import arrowSvg from '../../assets/icons/arrow-right.svg'
 import { RouterLink } from 'vue-router';
 import { WelcomeLayout } from './WelcomeLayout';
 export const First = defineComponent({
   setup: (props, context) => {
-    const slots = {
-      title: () => 'Simple way to manage you money',
-      subTitle: () => 'More than 100 million users with 1,000 thousand parthners services in the world',
-      buttons: () => <><RouterLink class={s.skip} to="/start">Skip</RouterLink>
-      <div class={s['ellipse-group']}>
-        <div class={[s.ellipse, s.active]}></div>
-        <div class={s.ellipse}></div>
-        <div class={s.ellipse}></div>
-      </div>
-      <RouterLink class={s.next} to="/welcome/2">
-        <img src={arrowSvg} alt="" />
-      </RouterLink></>
-    }
     return () => (
-        <WelcomeLayout v-slots={slots}></WelcomeLayout>
+        <WelcomeLayout>
+          {{
+            title: () => 'Simple way to manage you money',
+            subTitle: () => 'More than 100 million users with 1,000 thousand parthners services in the world',
+            buttons: () => <><RouterLink class={s.skip} to="/start">Skip</RouterLink>
+            <div class={s['ellipse-group']}>
+               <div class={[s.ellipse, s.active]}></div>
+              <div class={s.ellipse}></div>
+              <div class={s.ellipse}></div>
+            </div>
+            <RouterLink class={s.next} to="/welcome/2">
+              <img src={arrowSvg} alt="" />
+            </RouterLink></>
+          }}
+        </WelcomeLayout>
     )
   }
 })
--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,25 +1,26 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './layout.module.scss';
 import { RouterLink } from 'vue-router';
 import arrowSvg from '../../assets/icons/arrow-right.svg';
 import { WelcomeLayout } from './WelcomeLayout';
 export const Second = defineComponent({
   setup: (props, context) => {
-    const slots = {
-      title: () => 'Easily take control of your financial situation.',
-      subTitle: () => 'Join a global community of satisfied users.',
-      buttons: () => <><RouterLink class={s.skip} to="/start">Skip</RouterLink>
-      <div class={s['ellipse-group']}>
-        <div class={s.ellipse}></div>
-        <div class={[s.ellipse, s.active]}></div>
-        <div class={s.ellipse}></div>
-      </div>
-      <RouterLink class={s.next} to="/welcome/3">
-        <img src={arrowSvg} alt="" />
-      </RouterLink></>
-    }
     return () => (
-      <WelcomeLayout v-slots={slots}></WelcomeLayout>
+      <WelcomeLayout>
+        {{
+          title: () => 'Easily take control of your financial situation.',
+          subTitle: () => 'Join a global community of satisfied users.',
+          buttons: () => <><RouterLink class={s.skip} to="/start">Skip</RouterLink>
+          <div class={s['ellipse-group']}>
+            <div class={s.ellipse}></div>
+            <div class={[s.ellipse, s.active]}></div>
+            <div class={s.ellipse}></div>
+          </div>
+          <RouterLink class={s.next} to="/welcome/3">
+            <img src={arrowSvg} alt="" />
+          </RouterLink></>
+        }}
+      </WelcomeLayout>
     )
   }
 })
